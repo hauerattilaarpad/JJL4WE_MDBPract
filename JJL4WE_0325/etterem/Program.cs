248c751fe3f17c738135890d0948ba6415504672
hauerattilaarpad <90259862+hauerattilaarpad@users.noreply.github.com>
2026-04-27 15:22:47 +0200

    Add files via upload

---
+++ JJL4WE_0325/etterem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+
+        XDocument dokumentum = XDocument.Load("etterem.xml");
+        XElement gyoker = dokumentum.Descendants("vendeglatas").First();
+
+        Console.WriteLine("(0.) A teljes dokumentum:\n\n" + gyoker);
+
+        Console.WriteLine("(1.) Az ötcsillagos éttermek:\n");
+        var otCsillagosEttermek = gyoker.Descendants("etterem")
+            .Where(elem => elem.Descendants("csillag").First().Value == "5")
+            .ToList();
+
+        otCsillagosEttermek.ForEach(elem =>
+            Console.WriteLine(" - " + elem.Descendants("nev").First().Value)
+        );
+
+        Console.WriteLine("\n(2.) Melyik vendég, melyik étteremben, mit rendelt, mennyiért:\n");
+
+        var harmasJoin = gyoker.Descendants("rendeles")
+            .Select(elem =>
+            {
+                var vendegID = elem.Attribute("e_v_v").Value;
+                var vendeg = gyoker.Descendants("vendeg")
+                    .First(v => v.Attribute("vkod").Value == vendegID)
+                    .Descendants("nev").First().Value;
+
+                var etteremID = elem.Attribute("e_v_e").Value;
+                var etterem = gyoker.Descendants("etterem")
+                    .First(e => e.Attribute("ekod").Value == etteremID)
+                    .Descendants("nev").First().Value;
+
+                var rendeltEtel = elem.Descendants("etel").First().Value;
+                var osszeg = elem.Descendants("osszeg").First().Value;
+
+                return new
+                {
+                    Vendeg = vendeg,
+                    Etterem = etterem,
+                    Etel = rendeltEtel,
+                    Osszeg = osszeg
+                };
+            })
+            .ToList();
+
+        harmasJoin.ForEach(j =>
+            Console.WriteLine($"{j.Vendeg} - {j.Etterem} - {j.Etel} - {j.Osszeg}")
+        );
+
+        var atlagKoltes = gyoker.Descendants("rendeles")
+            .Select(r => r.Descendants("osszeg").First().Value)
+            .Average(o => double.Parse(o));
+
+        Console.WriteLine($"\n(3.) Az átlagos költés: {atlagKoltes}");
+
+        Console.WriteLine("\n(4.) Minden rendelés összegét megduplázom és mentem:\n");
+
+        gyoker.Descendants("rendeles")
+            .ToList()
+            .ForEach(r =>
+            {
+                var osszegElem = r.Descendants("osszeg").First();
+                var osszeg = double.Parse(osszegElem.Value);
+                osszegElem.Value = (osszeg * 2).ToString();
+            });
+
+        new XDocument(gyoker).Save("etterem_modositott.xml");
+        Console.WriteLine("etterem_modositott.xml létrehozva");
+
+        Console.WriteLine("\n(5.) Törlöm a 3 csillagos éttermeket és mentem:\n");
+
+        gyoker.Descendants("etterem")
+            .Where(e => e.Descendants("csillag").First().Value == "3")
+            .ToList()
+            .ForEach(e => e.Remove());
+
+        new XDocument(gyoker).Save("etterem_torolt.xml");
+        Console.WriteLine("etterem_torolt.xml létrehozva");
---
+++ JJL4WE_0325/own/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+    XDocument dokumentum = XDocument.Load("JJL4WE_XML1.xml");
+            XElement gyoker = dokumentum.Descendants("Reptér_JJL4WE").First();
+
+            Console.WriteLine("(0.) A teljes dokumentum:\n\n" + gyoker);
+            Console.WriteLine("\n(1.) A \"várható\" státuszú járatok célállomásai:\n");
+            var varhatoJaratok = gyoker.Descendants("Járat")
+                .Where(elem => elem.Descendants("státusz").First().Value == "várható")
+                .ToList();
+
+            varhatoJaratok.ForEach(elem =>
+                Console.WriteLine(" - " + elem.Descendants("célállomás").First().Value)
+            );
+
+            Console.WriteLine("\n(2.) Melyik utas, hova utazik és mennyibe került a jegye:\n");
+
+            var harmasJoin = gyoker.Descendants("Utasok")
+                .Select(utasElem =>
+                {
+
+                    var utasNev = utasElem.Descendants("név").First().Value;
+                    var jaratSzam = utasElem.Attribute("Járatszám").Value;
+                    var jarat = gyoker.Descendants("Járat")
+                        .First(j => j.Attribute("Járatszám").Value == jaratSzam)
+                        .Descendants("célállomás").First().Value;
+
+                    var jegySorszam = utasElem.Attribute("Jegysorszám").Value;
+                    var jegyAr = gyoker.Descendants("Jegy")
+                        .First(j => j.Attribute("Jegysorszám").Value == jegySorszam)
+                        .Descendants("ár").First().Value;
+
+                    return new
+                    {
+                        Utas = utasNev,
+                        Celallomas = jarat,
+                        Ar = jegyAr
+                    };
+                })
+                .ToList();
+
+            harmasJoin.ForEach(j =>
+                Console.WriteLine($"{j.Utas} - {j.Celallomas} - {j.Ar} Ft")
+            );
+
+            var atlagJegyAr = gyoker.Descendants("Jegy")
+                .Select(j => j.Descendants("ár").First().Value)
+                .Average(ar => double.Parse(ar));
+
+            Console.WriteLine($"\n(3.) Az átlagos jegyár: {atlagJegyAr:F0} Ft");
+
+            Console.WriteLine("\n(4.) Minden jegy árát megduplázom és mentem:\n");
+
+            gyoker.Descendants("Jegy")
+                .ToList()
+                .ForEach(j =>
+                {
+                    var arElem = j.Descendants("ár").First();
+                    var ar = double.Parse(arElem.Value);
+                    arElem.Value = (ar * 2).ToString();
+                });
+
+            new XDocument(gyoker).Save("repter_modositott.xml");
+            Console.WriteLine("repter_modositott.xml létrehozva");
+
+            Console.WriteLine("\n(5.) Törlöm a késő ('késik' státuszú) járatokat és mentem:\n");
+
+            gyoker.Descendants("Járat")
+                .Where(j => j.Descendants("státusz").First().Value == "késik")
+                .ToList()
+                .ForEach(j => j.Remove());
+
+            new XDocument(gyoker).Save("repter_torolt.xml");
+            Console.WriteLine("repter_torolt.xml létrehozva");
